38d833cca88306a9e4a0521c64cbc0d30363443b
Felix Alimin <felixalimin@gmail.com>
2024-05-20 16:13:11 -0400

    added read/write data.txt to keep a record of high score for the snake game

--- scoreboard.py
+++ scoreboard.py
@@ -4,7 +4,8 @@ class Scorebord(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.color("white")
         self.goto(0, 270)
@@ -19,6 +20,8 @@ class Scorebord(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
